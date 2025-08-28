c27e25d8a433c3df4d9a38197aae724cfaac2dca
DevSolex <devsolex6@gmail.com>
2025-08-28 19:56:57 +0100

    impliment some other functions

--- task1.py
+++ task1.py
@@ -12,14 +12,79 @@ def add_product(store_dict, name, price, quantity):
 		return f"PRODUCT {name} ADDED SUCCESSFUL!"
 			
 
-print(add_product(store, 'name', 500, 2))
-
 def update_stock(store_dict, name, quantity):
 	if name in store_dict:
 		store_dict[name] = {'quantity': quantity}
 		return f'PRODUCT {name} UPDATED SUCCESSFUL!'
 	else:
 		return f'PRODUCT DON"T EXIST!'
-print(update_stock(store, 'name', 3))
 
 
+def sell_product(store_dict, name, quantity):
+	if name not in store_dict:
+		return f'PRODUCT {name} NOT IN STORE!'
+	elif store_dict[name]['quantity'] < quantity:
+		return f'NO ENOGH PRODUCT OF {name} IN STORE'
+	else:
+		store_dict[name]['quantity'] -= quantity
+		total_price = store_dict[name]['price'] * quantity
+		return f'{quantity} of {name} sold for ${total_price}'
+
+def display_inventory(store_dict):
+	for product, details in store_dict.items():
+		print(f'{product} Price: ${details["price"]}, Quantity: {details["quantity"]}')
+
+
+#def most_expensive_product(store_dict):
+	
+
+
+
+
+def total_potential_sales(store_dict):
+	total = sum(details['price'] * details['quantity'] for details in store_dict.values())
+	return f'Total potentail sales (if everything is sold): ${total}'
+
+def start():
+	print('Welcome to online Store Inventory & Sales System')
+	while True:
+		print('''
+		OPTIONS:
+		1. Add a new product.
+		2. Update product.
+		3. Sell product.
+		4. View invention.
+		5. Most expensive product.
+		6. Total potential sales.
+		7. Exit
+		''')
+
+		option = int(input('Enter option:\n>>'))
+		if option == 1:
+			name = input('Enter name:\n>>')
+			price = float(input('Enter price:\n>>'))
+			quantity = int(input('Enter quantity:\n>>'))
+			print(add_product(store, name, price, quantity))
+		elif option == 2:
+			name = input('Enter name:\n>>')
+			quantity = int(input('Enter quantity:\n>>'))
+			print(update_stock(store, name, quantity))
+		elif option == 3:
+			name = input('Enter name:\n>>')
+			quantity = int(input('Enter quantity:\n>>'))
+			print(sell_product(store, name, quantity))
+		elif option == 4:
+			display_inventory(store)
+		elif option == 5:
+			print('most_expensive_product(store)')
+		elif option == 6:
+			print(total_potential_sales(store))
+		elif option == 7:
+			print('EXITING......\nGOODBYE!')
+			break
+		else:
+			print('INVALID OPTION..\nTRY AGAIN!.')
+
+
+
+start()
